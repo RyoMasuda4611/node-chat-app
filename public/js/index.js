67b0f4c733e05f5486beb5d0c422eccd80961abe
Ryo Masuda <mrra4611@gmail.com>
2018-02-03 09:13:07 +0900

    add createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    to: 'jen@example.jp',
-    text: 'hey'
-  });
 });
 
 socket.on('disconnect', function () {
