109a3ffc122d37a6a472d9fd5b871fdd31def5ce
Ryo Masuda <mrra4611@gmail.com>
2018-02-04 13:30:52 +0900

    Add mustache.js for message template

--- public/js/index.js
+++ public/js/index.js
@@ -9,22 +9,27 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('New message', message);
   var formattedTime = moment(message.createdAt).format('LT');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${formattedTime} :${message.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+      from: message.from,
+      text: message.text,
+      createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (location) {
     var formattedTime = moment(location.createdAt).format('LT');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>')
-    a.attr('href', location.url);
-    li.text(`${location.from}: ${formattedTime}`)
-    li.append(a);
-    jQuery('#messages').append(li);
-  });
+    var template = jQuery('#location-message-template').html(); 
+    // 文字型の引数のみ
+    var html = Mustache.render(template, {
+      from: location.from,
+      url: location.url,
+      createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
+});
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
