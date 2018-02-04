60e61e9561d3182aefbcb3b4033702bd88c79ec3
Ryo Masuda <mrra4611@gmail.com>
2018-02-04 14:08:51 +0900

    Scroll if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+function scrollBottom () {
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight ){
+      messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -17,6 +31,7 @@ socket.on('newMessage', function (message) {
       createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollBottom();
 });
 
 socket.on('newLocationMessage', function (location) {
@@ -29,6 +44,7 @@ socket.on('newLocationMessage', function (location) {
       createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
